497adb367964126ab5266d0ba9cba29549202216
arshqiii <marshq23@gmail.com>
2025-01-21 11:01:46 +0700

    setup script

---
+++ index.js
@@ -0,0 +1,23 @@
+const clear = document.querySelector("#clearBtn")
+const equal = document.querySelector("#equalBtn")
+const point = document.querySelector("#pointBtn")
+const display = document.querySelector(".cur-display")
+const past_display = document.querySelector(".past-display")
+
+let result = 0
+
+const add = function (a, b) {
+    return a + b;
+}
+
+const subtract = function (a, b) {
+    return a - b;
+}
+
+const multiply = function (a, b) {
+    return a * b;
+}
+
+const divide = function (a, b) {
+    return a / b;
+}
