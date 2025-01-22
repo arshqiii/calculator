6f30a56aa00788fda872022b10a1645bc3060889
arshqiii <marshq23@gmail.com>
2025-01-22 17:56:19 +0700

    made change to id & class

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+let result = 0
+let firstOperand = ""
+let secondOperand = ""
+let operator = null
+let showingResult = false
+
 const clear = document.querySelector("#clearBtn")
 const equal = document.querySelector("#equalBtn")
 const point = document.querySelector("#pointBtn")
 const curDisplay = document.querySelector(".cur-display")
 const prevDisplay = document.querySelector(".prev-display")
+const numberBtn = document.querySelectorAll("[#numberBtn]")
+const operatorBtn = document.querySelectorAll("[#operator]")
 
-let result = 0
+
 
 const add = function (a, b) {
     return a + b;
