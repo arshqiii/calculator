de875b76e36dd7bd34470449f9b423b3cfb78c52
arshqiii <marshq23@gmail.com>
2025-01-22 18:45:12 +0700

    can display numbers with button and clear display (can cause overflow display)

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
-let result = 0
-let firstOperand = ""
-let secondOperand = ""
-let operator = null
-let showingResult = false
+let result = 0;
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+let showingResult = false;
+let shouldReset = false;
 
-const clear = document.querySelector("#clearBtn")
-const equal = document.querySelector("#equalBtn")
-const point = document.querySelector("#pointBtn")
-const curDisplay = document.querySelector(".cur-display")
-const prevDisplay = document.querySelector(".prev-display")
-const numberBtn = document.querySelectorAll("[#numberBtn]")
-const operatorBtn = document.querySelectorAll("[#operator]")
+const clear = document.querySelector("#clearBtn");
+const equal = document.querySelector("#equalBtn");
+const point = document.querySelector("#pointBtn");
+const curDisplay = document.querySelector(".cur-display");
+const prevDisplay = document.querySelector(".prev-display");
+const numberBtn = document.querySelectorAll(".numberBtn");
+const operatorBtn = document.querySelectorAll(".operator");
 
+numberBtn.forEach((button) =>
+    button.addEventListener("click", () => displayNumber(button.value))
+);
 
+clear.addEventListener("click", clearDisplay);
+
+function displayNumber(num) {
+    if (curDisplay.textContent === "0" || shouldReset) {
+        resetDisplay();
+    }
+    curDisplay.textContent += num;
+}
+
+function resetDisplay() {
+    curDisplay.textContent = "";
+    showingResult = false;
+    shouldReset = false;
+}
+
+function clearDisplay() {
+    curDisplay.textContent = "0";
+    prevDisplay.textContent = "";
+    firstOperand = "";
+    secondOperand = "";
+    showingResult = false;
+    shouldReset = false;
+    operator = null;
+}
 
 const add = function (a, b) {
     return a + b;
-}
+};
 
 const subtract = function (a, b) {
     return a - b;
-}
+};
 
 const multiply = function (a, b) {
     return a * b;
-}
+};
 
 const divide = function (a, b) {
     return a / b;
-}
+};
